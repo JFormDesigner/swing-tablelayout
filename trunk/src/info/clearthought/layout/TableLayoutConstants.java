6b4829af0106f77e7c72664cba25da66c327f183
clearthought <clearthought@a0d8b903-29bf-ce94-a28c-8d40ef3fbe66>
2009-06-11 03:34:45 +0000

    Added new license.

--- trunk/src/info/clearthought/layout/TableLayoutConstants.java
+++ trunk/src/info/clearthought/layout/TableLayoutConstants.java
@@ -1,21 +1,103 @@
 /*
  * ====================================================================
  *
- * The Clearthought Software License, Version 1.0
+ * The Clearthought Software License, Version 2.0
  *
  * Copyright (c) 2001 Daniel Barbalace.  All rights reserved.
  *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
+ * Project maintained at https://tablelayout.dev.java.net
  *
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
+ * I. Terms for redistribution of original source and binaries
  *
- * 2. The original software may not be altered.  However, the classes
- *    provided may be subclasses as long as the subclasses are not
- *    packaged in the info.clearthought package or any subpackage of
- *    info.clearthought.
+ * Redistribution and use of unmodified source and/or binaries are
+ * permitted provided that the following condition is met:
+ *
+ * 1. Redistributions of original source code must retain the above
+ *    copyright notice and license.  You are not required to redistribute
+ *    the original source; you may choose to redistribute only the
+ *    binaries.
+ *
+ * Basically, if you distribute unmodified source, you meet
+ * automatically comply with the license with no additional effort on
+ * your part.
+ *
+ * II. Terms for distribution of derived works via subclassing and/or
+ *     composition.
+ *
+ * You may generate derived works by means of subclassing and/or
+ * composition (e.g., the Adaptor Pattern), provided that the following
+ * conditions are met:
+ *
+ * 1. Redistributions of original source code must retain the above
+ *    copyright notice and license.  You are not required to redistribute
+ *    the original source; you may choose to redistribute only the
+ *    binaries.
+ *
+ * 2. The original software is not altered.
+ *
+ * 3. Derived works are not contained in the info.clearthought
+ *    namespace/package or any subpackage of info.clearthought.
+ *
+ * 4. Derived works do not use the class or interface names from the
+ *    info.clearthought... packages
+ *
+ * For example, you may define a class with the following full name:
+ *    org.nameOfMyOrganization.layouts.RowMajorTableLayout
+ *
+ * However, you may not define a class with the either of the
+ * following full names:
+ *    info.clearthought.layout.RowMajorTableLayout
+ *    org.nameOfMyOrganization.layouts.TableLayout
+ *
+ * III. Terms for redistribution of source modified via patch files.
+ *
+ * You may generate derived works by means of patch files provided
+ * that the following conditions are met:
+ *
+ * 1. Redistributions of original source code must retain the above
+ *    copyright notice and license.  You are not required to
+ *    redistribute the original source; you may choose to redistribute
+ *    only the binaries resulting from the patch files.
+ *
+ * 2. The original source files are not altered.  All alteration is
+ *    done in patch files.
+ *
+ * 3. Derived works are not contained in the info.clearthought
+ *    namespace/package or any subpackage of info.clearthought.  This
+ *    means that your patch files must change the namespace/package
+ *    for the derived work.  See section II for examples.
+ *
+ * 4. Derived works do not use the class or interface names from the
+ *    info.clearthought... packages.  This means your patch files
+ *    must change the names of the interfaces and classes they alter.
+ *    See section II for examples.
+ *
+ * 5. Derived works must include the following disclaimer.
+ *    "This work is derived from Clearthought's TableLayout,
+ *     https://tablelayout.dev.java.net, by means of patch files
+ *     rather than subclassing or composition.  Therefore, this work
+ *     might not contain the latest fixes and features of TableLayout."
+ *
+ * IV. Terms for repackaging, transcoding, and compiling of binaries.
+ *
+ * You may do any of the following with the binaries of the
+ * original software.
+ *
+ * 1. You may move binaries (.class files) from the original .jar file
+ *    to your own .jar file.
+ *
+ * 2. You may move binaries from the original .jar file to other
+ *    resource containing files, including but not limited to .zip,
+ *    .gz, .tar, .dll, .exe files.
+ *
+ * 3. You may backend compile the binaries to any platform, including
+ *    but not limited to Win32, Win64, MAC OS, Linux, Palm OS, any
+ *    handheld or embedded platform.
+ *
+ * 4. You may transcribe the binaries to other virtual machine byte
+ *    code protocols, including but not limited to .NET.
+ *
+ * V. License Disclaimer.
  *
  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
  * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
